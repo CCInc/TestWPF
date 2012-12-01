35784b078284b0c5fd343c1e29c240c82c525804
Johan Larsson <johan_gavle1@hotmail.com>
2012-12-01 01:43:55 +0100

    Added NotifyPropertyChanged

--- CCWpf/CCWpf/ViewModel.cs
+++ CCWpf/CCWpf/ViewModel.cs
@@ -11,6 +11,16 @@ namespace CCWpf
         {
             Name = "DefaultName";
         }
-        public string Name { get; set; }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
     }
 }
