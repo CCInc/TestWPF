749b4f54c617f5686bce680323b9f243837d4d62
Johan Larsson <johan_gavle1@hotmail.com>
2013-01-02 02:10:42 +0100

    dsfgsf

--- CCWpf/CCWpfTests/StringParseTests.cs
+++ CCWpf/CCWpfTests/StringParseTests.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace CCWpfTests
 {
-    class StringParseTests
+    [TestFixture]
+    public class StringParseTests
     {
+        [Test]
+        public void GetnumberTest()
+        {
+            string s = " 123 ";
+            int number = GetNumber(s);
+            Assert.AreEqual(123,number);
+        }
+
+        public int GetNumber(string numberAsString)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
