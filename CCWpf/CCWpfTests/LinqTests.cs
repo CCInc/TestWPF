58a63e1e0e43a880af33dc112ba3e531c8300ba1
Johan Larsson <johan_gavle1@hotmail.com>
2013-01-02 02:55:27 +0100

    Linq

---
+++ CCWpf/CCWpfTests/LinqTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CCWpfTests
+{
+    [TestFixture]
+    class LinqTests
+    {
+        [Test]
+        public void TestNameTest()
+        {
+            var ints = new List<int> {1, 2, 3, 4, 5, 6};
+
+            var filtered = ints.Where(x => x >= ints.Max() - 2);
+
+            Assert.IsTrue(new[]{4,5,6}.SequenceEqual(filtered));
+        }
+    }
+}
